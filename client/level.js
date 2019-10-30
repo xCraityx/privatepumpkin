f331f629ca7d4fb7202ade294ff08dbad7c95e4b
flines <37344174+Fl1nns@users.noreply.github.com>
2019-10-30 18:38:48 -0300

    levels

--- client/level.js
+++ client/level.js
@@ -1,16 +1,12 @@
 setTimeout(level(), 300000)
-setTimeout(nohacks(), 100)
-
-if (localStorage.getItem("level") < 1) {
-	localStorage("level") = 1;
-}
+setTimeout(nohacks(), 999)
 
 function nohacks() {
 	document.getElementById("pumpkinlevel").innerHTML = localStorage.getItem("level");
 }
 
-function level() {
-  var mylevel = localStorage.getItem("level");
-  localStorage.setItem("level", (mylevel)+1);
-  document.getElementById("pumpkinlevel").innerHTML = localStorage.getItem("level");
+function level(){
+	localStorage.getItem("level")++;
+	localStorage.setItem("level", (localStorage.getItem("level"))+1);
+	document.getElementById("pumpkinlevel").innerHTML = localStorage.getItem("level");
 }
